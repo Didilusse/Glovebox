e249b23896578f0f6ce5afb98f4c484d3f20b1f3
Adil Rahmani <didilusse@gmail.com>
2026-05-05 19:15:07 -0400

    added a basic front end with the placeholder for the car make, model and year

---
+++ frontend/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <p>Enter Car Make: {{ make }}</p>
+<input v-model="make" placeholder="Toyota" />
+
+<div>Enter Car Model: {{ model }}</div>
+<input v-model="model" placeholder="Camry" />
+
+<div>Enter Car Year: {{ year }}</div>
+<input v-model="year" placeholder="2020" />
+
+<div> {{ selected }}</div>
+
+<button @click="handleCreateCar">Create Car</button>
+
+</template>
+
+
+<script setup>
+import { ref } from 'vue'
+
+const make = ref('')
+const model = ref('')
+const year = ref('')
+const selected = ref('')
+
+const handleCreateCar = () => {
+  
+}
+</script>
