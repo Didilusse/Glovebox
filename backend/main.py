0149c0e18ab12032bbf0f4cef12c8e41ddc4a488
Adil Rahmani <didilusse@gmail.com>
2026-05-09 13:32:09 -0400

    Fixed a CORS issue

--- backend/main.py
+++ backend/main.py
@@ -10,16 +10,21 @@ async def lifespan(app: FastAPI):
     await init_db()
     yield
 
+origins = [
+    "http://localhost:5173", 
+    "http://localhost:8080", 
+]
 
 app = FastAPI(title="Glovebox API", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"], 
+    allow_origins=origins, 
     allow_credentials=True,
     allow_methods=["*"], 
     allow_headers=["*"],
 )
+
 app.include_router(car_route.router)
 app.include_router(maintenance_logs.router)
 app.include_router(reminder.router)
